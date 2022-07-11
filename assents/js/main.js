7ba5d6b2af67d814f567aeaa6c532f75b771571f
Anmy Moreno <nayle2701@gmail.com>
2022-07-10 22:41:21 -0400

    portafolio1

---
+++ assents/js/main.js
@@ -0,0 +1,13 @@
+
+/* Menu Navbar */
+const btnOpen = document.querySelector('.hero button')
+const btnClose = document.querySelector('.navbar ul .icon-x')
+const menu = document.querySelector('.navbar ul')
+
+btnOpen.addEventListener('click', function () {
+  menu.classList.toggle('active')
+})
+
+btnClose.addEventListener('click', function (e) {
+  menu.classList.remove('active')
+})
