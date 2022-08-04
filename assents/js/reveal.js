8010ef47d6ad1cff728ad9e1ae7c78f098969486
Anmy Moreno <nayle2701@gmail.com>
2022-08-04 18:22:32 -0400

    reveal

---
+++ assents/js/reveal.js
@@ -0,0 +1,16 @@
+function reveal(){
+    let reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach((reveal) => {
+       let windowHeight = window.innerHeight;
+       let elementTop = reveal.getBoundingClientRect().top;
+       let elementVisible= 100;
+
+       if (elementTop < windowHeight - elementVisible){
+         reveal.classList.add("active");
+       }else {
+        reveal.classList.remove("active");
+       }
+    });
+}
+window.addEventListener("scroll",reveal);
