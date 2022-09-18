c8a04589574b8bf53a1b5fcafb3dd5fc7c12a678
Anmy Moreno <nayle2701@gmail.com>
2022-09-17 22:15:04 -0400

    addproyect

--- assents/js/main.js
+++ assents/js/main.js
@@ -12,4 +12,23 @@ btnClose.addEventListener('click', function (e) {
   menu.classList.remove('active')
 })
 
+/* Secciones con ID */
+window.addEventListener('scroll', function (e) {
+  const scrollY = window.pageYOffset
+
+  for (const section of sections) {
+    const sectionHeight = section.offsetHeight
+    const sectionTop = section.offsetTop - 58
+    const sectionId = section.getAttribute('id')
+
+    const current = document.querySelector('.hero__nav-link[href*=' + sectionId + ']')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      current.classList.add('active')
+    } else {
+      current.classList.remove('active')
+    }
+  }
+})
+
 
